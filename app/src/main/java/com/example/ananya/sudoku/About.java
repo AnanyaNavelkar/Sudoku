94d2057538fd86d195a1e1175e0ab32cd6b322bb
Ananya Navelkar <Ananya Navelkar>
2017-02-12 20:32:50 +0530

    Uploading

---
+++ app/src/main/java/com/example/ananya/sudoku/About.java
@@ -0,0 +1,20 @@
+package com.example.ananya.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ananya on 10-02-2017.
+ */
+
+public class About extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sudoku);
+    }
+
+
+}
